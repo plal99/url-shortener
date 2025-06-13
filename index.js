69ec2de578a4dccb54fc9ea75d76bf3f44751fb7
plal99 <lal.praveen123@gmail.com>
2025-06-13 15:02:31 +0530

    port 3001

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.get('/:shortId', async(req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
